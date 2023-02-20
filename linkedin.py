dd9df2fda2f121a75ee0e657e899834623fac3f5
Micky Rajkumar <pathourajkumar@gmail.com>
2023-02-20 10:41:29 +0530

    added command line arguments

--- linkedin.py
+++ linkedin.py
@@ -1,28 +1,33 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
 
-email = input('userName or email: ')
-passwd = input('password: ')
-options = webdriver.ChromeOptions()
-# options.headless = True
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--start-maximized")
+import sys
+from lib.accepts import accept
+from lib.requests import request
 
-driver = webdriver.Chrome(options=options)
+if __name__ == "__main__":
+    arguments = sys.argv
+    print(len(arguments))
+    if len(arguments) >1:
+        argument = arguments[1]
+    else:
+        print("Try `python linkedin.py -h' for more information.: ")
+        sys.exit()
+    if argument == "accept":
+        accept()
+    elif argument == 'request':
+        request()
+    elif argument == "-h":
+        print("Options and arguments:")
+        print("")
+        print("accept     :  accept all the available request")
+        print("request     :  send request")
+        print("to request you need to provide name or designation")
+        print("***** python linkedin.py request programmer *****")
+        print("add number of person you want to send request (optional) default(first page)")
+        print("***** python linkedin.py request programmer 5 *****")
 
-driver.get('https://www.linkedin.com/')
-driver.implicitly_wait(15)
-userName = driver.find_element(By.ID,'session_key')
-userName.send_keys(email)
-password = driver.find_element(By.ID,'session_password')
-password.send_keys(passwd)
-signIn = driver.find_element(By.CLASS_NAME,'sign-in-form__submit-button')
-signIn.click()
-driver.implicitly_wait(15)
-network = driver.find_element(By.LINK_TEXT, 'My Network')
-network.click()
+    else:
+        print("Unknown option: ", argument)
+        print("Try `python linkedin.py -h' for more information.: ")
 
 
-sleep(10)
 
