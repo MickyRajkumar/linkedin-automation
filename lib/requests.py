27ea12e2d9c7360c3a383a4b746cbdbba0e42ec8
Micky Rajkumar <pathourajkumar@gmail.com>
2023-02-20 19:16:34 +0530

    added search functionallity

--- lib/requests.py
+++ lib/requests.py
@@ -6,10 +6,10 @@ from selenium.webdriver.common.keys import Keys
 def request(search):
     # print("request")
     # print(search)
-    # email = input('userName or email: ')
-    # passwd = input('password: ')
-    email = 'mickyrajkumar@gmail.com'
-    passwd = "R@jkum@r4798"
+    email = input('userName or email: ')
+    passwd = input('password: ')
+    # email = 'mickyrajkumar@gmail.com'
+    # passwd = "R@jkum@r4798"
     options = webdriver.ChromeOptions()
     # options.headless = True
     options.add_argument("--window-size=1920x1080")
@@ -19,6 +19,7 @@ def request(search):
 
     driver.get('https://www.linkedin.com/')
     driver.implicitly_wait(15)
+
     userName = driver.find_element(By.ID,'session_key')
     userName.send_keys(email)
     password = driver.find_element(By.ID,'session_password')
@@ -35,6 +36,11 @@ def request(search):
     # peopleButton = driver.find_element(By.LINK_TEXT, 'People')
     # print("people",peopleButton[0])
     peopleButton[0].click()
+    html = driver.find_element(By.TAG_NAME, 'html')
+    html.send_keys(Keys.END)
+    nextPage = driver.find_elements(By.CLASS_NAME,'artdeco-pagination__indicator')
+    print('page: ---', nextPage )
+    # nextPage[1].click()
 
 
     sleep(30)
